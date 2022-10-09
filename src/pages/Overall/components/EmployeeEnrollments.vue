e1b8f5598e96e834dff4ada9b0cf00c79ae46dc8
nkubito-pacis <pacisnkubito@gmail.com>
2022-10-09 07:52:40 +0200

    Create EmployeeEnrollments.vue

---
+++ src/pages/Overall/components/EmployeeEnrollments.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script setup lang="ts">
+  import { onMounted, ref } from 'vue';
+  import {getEmployeesEnrollmentsData} from '../data'
+
+  const chartData = ref({
+    labels: [''],
+    datasets: [
+      {
+        data: [0]
+      }
+    ]
+  })
+
+  function loadComponent() {
+    getEmployeesEnrollmentsData()
+    .then((data)=>chartData.value=data)
+  }
+
+  onMounted(()=>{
+    loadComponent()
+  })
+
+</script>
