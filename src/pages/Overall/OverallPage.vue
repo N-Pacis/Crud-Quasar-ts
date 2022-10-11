05f927e86f52ca41e7ff7fa4959927ed52d3771c
nkubito-pacis <pacisnkubito@gmail.com>
2022-10-11 07:52:52 +0200

    Create OverallPage.vue

---
+++ src/pages/Overall/OverallPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="full-width columns items-center q-pa-lg">
+    <div class="full-width q-pa-md">
+      <Employee />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import Employee from './components/Employee.vue';
+</script>
